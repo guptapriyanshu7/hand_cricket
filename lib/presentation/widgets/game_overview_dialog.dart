cb1ec211ed67a11b4431a635d702df186d783508
Priyanshu Gupta <guptapriyanshu71@gmail.com>
2025-04-24 13:47:34 +0530

    Refactor gameOverviewDialog to remove unnecessary post-frame callback and streamline dialog display logic for improved readability and maintainability.
    Signed-off-by: Priyanshu Gupta <guptapriyanshu71@gmail.com>

--- lib/presentation/widgets/game_overview_dialog.dart
+++ lib/presentation/widgets/game_overview_dialog.dart
@@ -4,70 +4,67 @@ import 'package:hand_cricket/presentation/providers/game_provider.dart';
 import 'package:hand_cricket/presentation/widgets/game_result_widget.dart';
 import 'package:provider/provider.dart';
 
-// to show six, out, to defend statuses
+// show final result of the game
 void gameOverviewDialog(
   BuildContext context,
   OverlayEvent gameResult,
   String? playerScore,
 ) {
-  WidgetsBinding.instance.addPostFrameCallback((_) {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) {
-        return Dialog(
-          backgroundColor: Colors.transparent.withValues(alpha: 0.8),
-          insetPadding: EdgeInsets.all(0),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-          elevation: 0,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (gameResult == OverlayEvent.gameLost ||
-                  gameResult == OverlayEvent.timeOut) ...[
-                GameResultWidget(result: 'You lose!'),
-                if (gameResult == OverlayEvent.timeOut)
-                  Text(
-                    'Clock ran out!',
-                    style: Theme.of(
-                      context,
-                    ).textTheme.bodyMedium?.copyWith(fontSize: 24.sp),
-                  ),
-                SizedBox(height: 20.h),
-              ] else if (gameResult == OverlayEvent.gameWon)
-                Padding(
-                  padding: const EdgeInsets.all(12.0).r,
-                  child: Column(
-                    children: [
-                      Image.asset('assets/you_won.png'),
-                      Text(
-                        'Your score: $playerScore',
-                        style: Theme.of(
-                          context,
-                        ).textTheme.bodyMedium?.copyWith(fontSize: 24.sp),
-                      ),
-                    ],
-                  ),
-                )
-              else
-                GameResultWidget(result: 'Scores tied!'),
-              SizedBox(height: 20.h),
-              TextButton(
-                onPressed: () {
-                  context.read<GameProvider>().reset();
-                  Navigator.of(context).pop();
-                },
-                child: Text(
-                  'Play Again',
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) {
+      return Dialog(
+        backgroundColor: Colors.transparent.withValues(alpha: 0.8),
+        insetPadding: EdgeInsets.all(0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        elevation: 0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (gameResult == OverlayEvent.gameLost ||
+                gameResult == OverlayEvent.timeOut) ...[
+              GameResultWidget(result: 'You lose!'),
+              if (gameResult == OverlayEvent.timeOut)
+                Text(
+                  'Clock ran out!',
                   style: Theme.of(
                     context,
                   ).textTheme.bodyMedium?.copyWith(fontSize: 24.sp),
                 ),
+              SizedBox(height: 20.h),
+            ] else if (gameResult == OverlayEvent.gameWon)
+              Padding(
+                padding: const EdgeInsets.all(12.0).r,
+                child: Column(
+                  children: [
+                    Image.asset('assets/you_won.png'),
+                    Text(
+                      'Your score: $playerScore',
+                      style: Theme.of(
+                        context,
+                      ).textTheme.bodyMedium?.copyWith(fontSize: 24.sp),
+                    ),
+                  ],
+                ),
+              )
+            else
+              GameResultWidget(result: 'Scores tied!'),
+            SizedBox(height: 20.h),
+            TextButton(
+              onPressed: () {
+                context.read<GameProvider>().reset();
+              },
+              child: Text(
+                'Play Again',
+                style: Theme.of(
+                  context,
+                ).textTheme.bodyMedium?.copyWith(fontSize: 24.sp),
               ),
-            ],
-          ),
-        );
-      },
-    );
-  });
+            ),
+          ],
+        ),
+      );
+    },
+  );
 }
